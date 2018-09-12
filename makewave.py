407fa7a224e14404145b506284b0c90d1d1f8732
William Morgan <bill@mrgn.org>
2018-09-11 20:55:22 -0700

    Added ADSR envelope functionality and preliminary version of note representation system required to use it.

--- makewave.py
+++ makewave.py
@@ -21,6 +21,70 @@ def lerp(a, b, t):
     """
     return a + (b-a) * t
 
+def adsr(attack, decay, sustain, release, samples_per_second):
+    """
+    Returns a generator object that produces a series of values
+    ramping over time from 0 to 1 to the sustain level and back to 0.
+    attack, decay, and release are measured in seconds.
+    Send True to the generator to signal the note has been released.
+    """
+    value = 0.0
+    released = None
+    attack_rate = 1.0 / (attack * samples_per_second)
+    decay_rate = (1.0 - sustain) / (decay * samples_per_second)
+    release_rate = sustain / (release * samples_per_second)
+    while value < 1.0 and not released:
+        value += attack_rate
+        released = (yield value)
+    while value > sustain and not released:
+        value -= decay_rate
+        released = (yield value)
+    while not released:
+        released = (yield value)
+    while value > 0.0:
+        value -= release_rate
+        yield value
+
+from enum import Enum
+class Commands(Enum):
+    set_volume = 1
+    change_volume = 2
+    set_pitch = 3
+    change_pitch = 4
+    release = 5
+    stop = 6
+
+def sine(commands, samples_per_second):
+    time_between_calls = 1.0 / samples_per_second
+    time_in_seconds = 0.0
+    volume = 1.0
+    midi_note_number = 69
+    my_adsr = adsr(0.01, 0.01, 0.75, 0.1, samples_per_second)
+    released = None
+    while True:
+        if commands is not None:
+            for command, param in commands:
+                if (command == Commands.set_volume):
+                    volume = param
+                elif (command == Commands.change_volume):
+                    volume += param
+                elif (command == Commands.set_pitch):
+                    midi_note_number = param
+                elif (command == Commands.change_pitch):
+                    midi_note_number += param
+                elif (command == Commands.release):
+                    released = True
+                elif (command == Commands.stop):
+                    raise StopIteration
+
+        frequency = mtof(midi_note_number)
+        sample = math.sin(math.pi * 2 * time_in_seconds * frequency)
+        sample *= volume
+        sample *= my_adsr.send(released)
+
+        commands = (yield sample)
+        time_in_seconds += time_between_calls
+
 class Sine:
     def render(self, time_in_seconds, midi_note_number):
         frequency = mtof(midi_note_number)
@@ -50,12 +114,52 @@ class Noise:
     def render(self, time_in_seconds, midi_note_number):
         return random.uniform(-1.0, 1.0)
 
-instrument = Noise();
+instrument = Sine()
 notes = [0, 4, 7, 0 + 12, 4 + 12, 7 + 12, 24, 7 + 12, 4 + 12, 12, 7, 4]
+major_scale_notes = [0, 2, 4, 5, 7, 9, 11]
+major_triad = [0, 4, 7]
+notes = major_scale_notes + [12, 14, 12] + major_scale_notes[::-1] 
+#notes = [0, 2, 3, 5, 7]
+
+
 
 samples_per_second = 44100
 song_length_seconds = 2
 data = bytearray()
+
+timed_commands = []
+for i in range(len(notes)):
+    note_on_command = (Commands.set_pitch, notes[i] + 69.0)
+    timed_note_on_command = (i * 44100 / 4, note_on_command)
+    timed_commands.append(timed_note_on_command)
+    note_off_command = (Commands.release, None)
+    timed_note_off_command = ((i * 44100 / 4) + (44100 / 8), note_off_command)
+    timed_commands.append(timed_note_off_command)
+
+current_sample = 1
+voices = [];
+while len(timed_commands) > 0 or len(voices) > 0:
+    command = None
+    if len(timed_commands) > 0 and timed_commands[0][0] < current_sample:
+        timed_command = timed_commands.pop(0)
+        if timed_command[1][0] == Commands.set_pitch:
+            voices.append(sine([timed_command[1]], samples_per_second))
+            command = None
+        else:
+            command = [timed_command[1]]
+    sample = 0
+    for voice in voices:
+        try:
+            sample += voice.send(command)
+        except StopIteration:
+            voices = []
+    unitless = sample / 2.0 + .5
+    value = unitless * 255
+    trimmed = min(max(value, 0), 255)
+    data.append(int(trimmed))
+    current_sample += 1
+
+'''
 for x in range(0, song_length_seconds * samples_per_second):
     time_in_seconds = x / samples_per_second
     progress_through_song = time_in_seconds / song_length_seconds
@@ -64,7 +168,7 @@ for x in range(0, song_length_seconds * samples_per_second):
     unitless = positive_or_negative / 2.0 + .5
     value = unitless * 255
     data.append(int(value))
-
+'''
 wave_write = wave.open('billtest.wav', mode='wb')
 wave_write.setnchannels(1)
 wave_write.setframerate(samples_per_second)
--- tests.py
+++ tests.py
@@ -6,13 +6,13 @@ class MyTest(unittest.TestCase):
         self.assertEqual(makewave.mtof(69.0), 440.0)
 
     def test_mtof_middle_c(self):
-        self.assertEqual(makewave.mtof(60.0), 261.6255653005986)
+        self.assertAlmostEqual(makewave.mtof(60.0), 261.63, places = 2)
 
     def test_ftom_middle_a(self):
         self.assertEqual(makewave.ftom(440.0), 69.0)
 
     def test_ftom_middle_c(self):
-        self.assertEqual(makewave.ftom(261.6255653005986), 60.0)
+        self.assertAlmostEqual(makewave.ftom(261.63), 60.0, places = 2)
 
     def test_lerp(self):
         self.assertEqual(makewave.lerp(10, 20, -0.5), 5)
@@ -20,6 +20,28 @@ class MyTest(unittest.TestCase):
         self.assertEqual(makewave.lerp(10, 20,  0.5), 15)
         self.assertEqual(makewave.lerp(10, 20,  1.0), 20)
         self.assertEqual(makewave.lerp(10, 20,  1.5), 25)
+
+    def test_adsr(self):
+        test_adsr = makewave.adsr(0.5, 0.5, 0.75, 0.5, 10)
+        self.assertAlmostEqual(test_adsr.__next__(), 0.2,  places = 2)
+        self.assertAlmostEqual(test_adsr.__next__(), 0.4,  places = 2)
+        self.assertAlmostEqual(test_adsr.__next__(), 0.6,  places = 2)
+        self.assertAlmostEqual(test_adsr.__next__(), 0.8,  places = 2)
+        self.assertAlmostEqual(test_adsr.__next__(), 1.0,  places = 2)
+        self.assertAlmostEqual(test_adsr.__next__(), 0.95, places = 2)
+        self.assertAlmostEqual(test_adsr.__next__(), 0.90, places = 2)
+        self.assertAlmostEqual(test_adsr.__next__(), 0.85, places = 2)
+        self.assertAlmostEqual(test_adsr.__next__(), 0.80, places = 2)
+        self.assertAlmostEqual(test_adsr.__next__(), 0.75, places = 2)
+        self.assertAlmostEqual(test_adsr.__next__(), 0.75, places = 2)
+        self.assertAlmostEqual(test_adsr.__next__(), 0.75, places = 2)
+        self.assertAlmostEqual(test_adsr.send(True), 0.60, places = 2)
+        self.assertAlmostEqual(test_adsr.__next__(), 0.45, places = 2)
+        self.assertAlmostEqual(test_adsr.__next__(), 0.30, places = 2)
+        self.assertAlmostEqual(test_adsr.__next__(), 0.15, places = 2)
+        self.assertAlmostEqual(test_adsr.__next__(), 0.0, places = 2)
+        with self.assertRaises(StopIteration):
+            test_adsr.__next__()
 
 if __name__ == '__main__':
     unittest.main()
