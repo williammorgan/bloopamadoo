c93bba585d89c3438836bba4a1864da391f5e3b5
William Morgan <bill@mrgn.org>
2018-09-14 19:36:18 -0700

    Added separate arpeggio and melody tracks.

--- makewave.py
+++ makewave.py
@@ -109,24 +109,26 @@ class Noise:
 #notes = [0, 4, 7, 0 + 12, 4 + 12, 7 + 12, 24, 7 + 12, 4 + 12, 12, 7, 4]
 major_scale_notes = [0, 2, 4, 5, 7, 9, 11]
 major_triad = [0, 4, 7]
-#notes = major_scale_notes + [12, 14, 12] + major_scale_notes[::-1] 
 #notes = [0, 2, 3, 5, 7]
-notes = major_triad * 6
-#make notes absoulute from middle a, instead of relative.
-notes = [x + 69 for x in notes]
+root_note = 69
 
-
-samples_per_second = 44100
-data = bytearray()
-
-timed_commands = []
-voices = []
+arpeggio_notes = major_triad * 24
+melody_notes = major_scale_notes + [12, 14, 12] + major_scale_notes[::-1]
+#make note numbers absoulute from the root, instead of relative.
+arpeggio_notes = [x + root_note for x in arpeggio_notes]
+melody_notes = [x + root_note for x in melody_notes]
 
 beat_pattern = [BassDrum, None,     None,     None,
                 Noise,    None,     BassDrum, BassDrum,
                 BassDrum, None,     BassDrum, None,
                 Noise,    None,     None,     None]
 beat = beat_pattern * 2
+
+samples_per_second = 44100
+data = bytearray()
+timed_commands = []
+voices = []
+
 for i in range(len(beat)):
     if beat[i] is None:
         continue;
@@ -141,10 +143,24 @@ for i in range(len(beat)):
     timed_note_off_command = ((i * 44100 / 8) + (44100 / 16), note_off_command)
     timed_commands.append(timed_note_off_command)
 
-for i in range(len(notes)):
+for i in range(len(arpeggio_notes)):
     voice = Voice(Saw(), samples_per_second)
-    def note_on_command(voice = voice, i = i): # I don't like that I have to do this for the closure to work correctly
-        voice.set_pitch(notes[i])
+    def note_on_command(voice = voice, i = i):
+        voice.set_pitch(arpeggio_notes[i])
+        voice.set_volume(.025)
+        voice.adsr = adsr_generator(0.0001, 0.0001, 1.0, 0.0001, samples_per_second)
+        voices.append(voice)
+    timed_note_on_command = (i * 44100 / 24, note_on_command)
+    timed_commands.append(timed_note_on_command)
+    def note_off_command(voice = voice):
+        voice.release()
+    timed_note_off_command = ((i * 44100 / 24) + (44100 / 24), note_off_command)
+    timed_commands.append(timed_note_off_command)
+
+for i in range(len(melody_notes)):
+    voice = Voice(Saw(), samples_per_second)
+    def note_on_command(voice = voice, i = i):
+        voice.set_pitch(melody_notes[i])
         voice.set_volume(.05)
         voices.append(voice)
     timed_note_on_command = (i * 44100 / 4, note_on_command)
@@ -163,7 +179,6 @@ while len(timed_commands) > 0 or len(voices) > 0:
         timed_command[1]()
 
     sample = 0.0
-
     new_voices = []
     for voice in voices:
         try:
